5484fb928df52bba36371a82743e19bf0816c6f6
Radu Scrobota <raduscrobotapp@gmail.com>
2022-11-16 12:43:17 +0100

    actualizaciones generales 16/11/2022

--- movielens.R
+++ movielens.R
@@ -26,6 +26,10 @@ r <- read_csv("ml-25m/ratings.csv")  %>%  as_tibble()
 t <- read_csv("ml-25m/tags.csv")  %>%  as_tibble()
 l <- read_csv("ml-25m/links.csv")  %>%  as_tibble()
 
+##Esto te limpia el dataset de dobles espacios antes de usar las expresiones regulares
+str_squish()
+
+
 
 # STATS,  EDA, PLAYING AROUND ---------------------------------------------
 
@@ -40,7 +44,35 @@ m %>%  glimpse()
 
 # extraemos de titulo y lo añadimos en una nueva columna llamada year
 # despues de c("year") ponemos la expresion regular
-m %>% tidyr::extract(title, c("title1", "year"), "(.*) \\(([0-9]*)", remove = F)
+m %<>% tidyr::extract(title, c("title1", "year"), "(.*) \\(([0-9]*)", remove = F)
+# cambiamos el tipo de dato de year para que sea integer
+m %<>%  mutate(year=as.integer(year)) 
+
+
+
+
+# ESTADISTICAS BASICAS DE PELICULAS ---------------------------------------
+
+## año cuando se han hecho mas peliculas
+m %>% count(year, sort = T) 
+
+## año cuando menos
+m %>% count(year) 
+
+## cuantos NA hay en el dataset de movies
+sum(is.na(m))
+
+## Grafico de como han crecido o decrecido las peliculas por año 
+
+m %>% drop_na() %>% ggplot(aes(x = year, y=count))+ geom_line()
+
+## Marcas
+## gasolineras famosa (repsol) o acompañado de SA , S.A
+
+## Franquicia
+## Las demás
+
+
 
 
 
